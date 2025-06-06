fe264a6e5a423d079e8cc07b88afa2a95e6fdd2f
Dean Hauser <63527400+IzziCooki@users.noreply.github.com>
2025-06-06 16:26:18 -0400

    Update firebase.js
    remove api keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,13 +9,13 @@ import {
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlCs8iIlsSGwcz7h_cwtdyASchpUYSzro",
-  authDomain: "agency-c7a0f.firebaseapp.com",
-  projectId: "agency-c7a0f",
-  storageBucket: "agency-c7a0f.firebasestorage.app",
-  messagingSenderId: "206001569414",
-  appId: "1:206001569414:web:d0d41e5ad7d9aadb641b07",
-  measurementId: "G-JBQ6W96G3Z"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
